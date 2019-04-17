de60b8b86a24162d9a92aa3b96c1e9a57dca69b0
guobaolun <398464028@qq.com>
2019-04-18 00:17:02 +0800

    待完善

--- app/src/main/java/com/snowy/ttword/fragment/MeFragment.java
+++ app/src/main/java/com/snowy/ttword/fragment/MeFragment.java
@@ -6,8 +6,12 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
 
+import com.snowy.common.dialog.BottomSlideDialog;
 import com.snowy.ttword.R;
+import com.snowy.widget.ItemView;
 
 /**
  * @author guobaolun
@@ -19,7 +23,29 @@ public class MeFragment extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         super.onCreateView(inflater, container, savedInstanceState);
-
-        return inflater.inflate(R.layout.fragment_study, container,false);
+        View view = inflater.inflate(R.layout.fragment_study, container,false);
+        Button button = view.findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showPortraitDialog();
+            }
+        });
+        return view;
     }
+
+
+    public void showPortraitDialog() {
+        View view = View.inflate(getActivity(), R.layout.dialog_logout, null);
+
+
+        BottomSlideDialog  mBottomSlideDialog = new BottomSlideDialog(getActivity(), R.style.ActionSheetDialogStyle);
+        mBottomSlideDialog.setContentView(view);
+        mBottomSlideDialog.show();
+    }
+
+
+
+
+
 }
